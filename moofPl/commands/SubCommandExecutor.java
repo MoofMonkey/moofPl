5bd89d0e8b238373f7f114991d29e13ede220f3c
Nikita <tyumencev2004@bk.ru>
2016-12-29 15:15:05 +0300

    Forgot update :C

--- moofPl/commands/SubCommandExecutor.java
+++ moofPl/commands/SubCommandExecutor.java
@@ -1,10 +1,11 @@
 package moofPl.commands;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import moofPl.Main;
 
 public class SubCommandExecutor {
-	public void handle(Main main, Player p, String commandLabel,
-			String[] args) {  }
+	public void handle(Main main, Player p, CommandSender sender,
+			String commandLabel, String[] args) {  }
 }
